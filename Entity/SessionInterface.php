d0d5e4b65f5091254b7da781a94bc4225352ee21
Romain <romain.sanchez@libre-informatique.fr>
2017-02-16 16:00:35 +0100

    Defined SessionInterface

--- Entity/SessionInterface.php
+++ Entity/SessionInterface.php
@@ -4,5 +4,48 @@ namespace Blast\DoctrineSessionBundle\Entity;
 
 interface SessionInterface
 {
-   
+   /**
+     * Set sessionId
+     *
+     * @param string $sessionId
+     * @return Session
+     */
+    public function setSessionId($sessionId);
+
+    /**
+     * Get sessionId
+     *
+     * @return string 
+     */
+    public function getSessionId();
+
+    /**
+     * Set data
+     *
+     * @param string $data
+     * @return Session
+     */
+    public function setData($data);
+
+    /**
+     * Get data
+     *
+     * @return string 
+     */
+    public function getData();
+    
+    /**
+     * Set expiresAt
+     *
+     * @param string $expiresAt
+     * @return Session
+     */
+    public function setExpiresAt($expiresAt);
+
+    /**
+     * Get expiresAt
+     *
+     * @return string 
+     */
+    public function getExpiresAt();
 }
