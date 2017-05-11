9cd4e54110b0fd83271fab19625cd0168a2e82f4
François GERBES <francois.gerbes@libre-informatique.fr>
2017-05-11 17:02:56 +0200

    Try on postgresql database creation

--- bin/ci-scripts/create_travis_database_test.sh
+++ bin/ci-scripts/create_travis_database_test.sh
@@ -22,9 +22,11 @@ mysql -u travis -e 'CREATE DATABASE travis;' -v
 
 # (we try to create a travis user)
 # psql -c 'CREATE USER travis;' -U postgres
-psql -c 'ALTER ROLE travis WITH CREATEDB;' -U postgres
+# psql -c 'ALTER ROLE travis WITH CREATEDB;' -U postgres
 
-psql -c 'CREATE DATABASE travis;' -U travis
+psql -c 'CREATE DATABASE travis;' -U postgres
+psql -c 'ALTER DATABASE travis OWNER TO travis' -U postgres
+
 
 #psql -U postgres -c "CREATE EXTENSION 'uuid-ossp';"
 
