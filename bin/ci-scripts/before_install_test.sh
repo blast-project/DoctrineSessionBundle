49825e766aaa6c80a3b440f9c43e24927dc16d32
François GERBES <francois.gerbes@libre-informatique.fr>
2017-05-11 15:02:04 +0200

    Split database creation from other process

--- bin/ci-scripts/before_install_test.sh
+++ bin/ci-scripts/before_install_test.sh
@@ -7,9 +7,3 @@ set -ev
 composer self-update
 sed --in-place "s/\"dev-master\":/\"dev-${TRAVIS_COMMIT}\":/" composer.json
 
-# Database creation
-# doto add db in a env variable and create the good one
-
-mysql -u travis -e 'CREATE DATABASE travis;' -v
-
-psql -c 'create database travis;' -U postgres
